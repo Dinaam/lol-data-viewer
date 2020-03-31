2816f75aad0577232d34b189b528e86761f42d34
unknown <dinaam.n@gmail.com>
2020-03-31 16:54:39 +0200

    config dist updated

--- backend/config/config.dist.js
+++ backend/config/config.dist.js
@@ -2,6 +2,6 @@ module.exports = {
     DB: process.env.MONGO_URL ? process.env.MONGO_URL : 'MONGO_URL',
     APP_PORT: process.env.APP_PORT ? process.env.APP_PORT : 80,
     API_ID: "",
-    API_KEY:'RGAPI-0d1ec249-c5dc-4205-b1c9-b6afba27d39d'
+    API_KEY:''
   };
   
